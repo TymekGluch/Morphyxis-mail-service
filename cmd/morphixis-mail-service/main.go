babc789c80ac116d8c126ff05dd6e58213ba0978
Tymoteusz GŁuch <gluchtymoteusz@gmail.com>
2026-06-10 11:31:31 +0200

    feat: Add new version

--- cmd/morphixis-mail-service/main.go
+++ cmd/morphixis-mail-service/main.go
@@ -1,32 +1,9 @@
 package main
 
 import (
-	mailcowIntegration "Morphyxis-mail-service/internal/mailcow-integration"
-	"context"
 	"fmt"
 )
 
 func main() {
 	fmt.Print("Morphyxis-mail-service is running... \n")
-
-	ctx := context.Background()
-	mailClient, err := mailcowIntegration.NewClient(ctx)
-	if err != nil {
-		fmt.Println("Error creating mail client:", err)
-		return
-	}
-
-	err = mailClient.SendDeletedAccountEmail(mailcowIntegration.SendDeletedAccountEmailInput{
-		To:      "tymoteuszgluch848@gmail.com",
-		Subject: "Account Deleted",
-		Name:    "test user",
-		Reason:  "User requested account deletion",
-	})
-	if err != nil {
-		fmt.Println("Error sending deleted account email:", err)
-		return
-	} else {
-		fmt.Println("Deleted account email sent successfully!")
-	}
-
 }
