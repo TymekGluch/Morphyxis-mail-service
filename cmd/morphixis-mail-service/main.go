8c6aa90c3d6d9d0bc0b2b2cd319f6384a9313c44
Tymoteusz GŁuch <gluchtymoteusz@gmail.com>
2026-07-12 20:12:16 +0200

    chore: update version in ./cmd/morphixis-mail-service/main.go to 1.1.1

--- cmd/morphixis-mail-service/main.go
+++ cmd/morphixis-mail-service/main.go
@@ -21,7 +21,7 @@ const (
 )
 
 // @title Morphyxis-mail-service API
-// @version 1.1.0
+// @version 1.1.1
 // @description Morphyxis-mail-service API documentation
 // @BasePath /api
 // @schemes https http
